69d17bd2af7a87dd9e9f86a2f1e85094b6f74678
Simon Wenczowski <simon.wenczowski@tum.de>
2023-12-29 15:33:40 +0100

    adding minimal test case (1 core, 12s)

---
+++ tests/GeostrophicWind/check.py
@@ -0,0 +1,18 @@
+# 
+# Checking if the geostrophic equlibrium between Coriolis "force" 
+# and pressure gradient has been maintained throughout the run.
+#
+logfile = open('LOGS/uvwbulk.log', 'r')
+lines = logfile.readlines()
+
+# ubulk is the third entry in the log file
+nubulk = 2
+lubulk = []
+
+# reading lines and extracting ubulk
+for line in lines[1:]:
+    lubulk.append( float(line.split()[nubulk]) )  
+
+# throwing error if ubulk has changed
+if ( max(lubulk) - min( lubulk ) ) > 0.00001:
+  raise ValueError("GeostrophicWind: ubulk should not change during the test. Error.")
