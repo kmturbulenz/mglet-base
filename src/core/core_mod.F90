0936b64eb42e69ffe3a1af67977fac105238b153
Håkon Strandenes <h.strandenes@km-turbulenz.no>
2024-04-05 12:18:31 +0200

    Add the possibility to set FPE traps and underflow mode via environment variables
    Also explicitly disable FPE traps during MPI_Init and h5open_f, ref for example:
    https://github.com/HDFGroup/hdf5/issues/3831

--- src/core/core_mod.F90
+++ src/core/core_mod.F90
@@ -49,19 +49,47 @@ MODULE core_mod
 
     CHARACTER(len=*), PARAMETER :: logdir = "LOGS"
 
+    ! These are not to be called from other places in the code and are PRIVATE
+    PRIVATE :: set_fpe_traps, set_underflow_mode, print_ieee_modes
+
 CONTAINS
     ! Call all initializer-functions in the core
     SUBROUTINE init_core()
         USE HDF5, ONLY: h5open_f
         USE MPI_f08, ONLY: MPI_Init
+        USE, INTRINSIC :: IEEE_EXCEPTIONS
 
         ! Local variables
         INTEGER(int32) :: ierr
+        LOGICAL :: saved_fpe_mode(SIZE(ieee_all))
 
+        ! Fetch the IEEE halting modes and disable all of them temporarily for
+        ! the MPI_Init and h5open_f calls. This is to avoid the MPI and HDF5
+        ! libraries to trigger floating point exceptions (sometimes they do)
+        CALL IEEE_GET_HALTING_MODE(IEEE_ALL, saved_fpe_mode)
+        CALL IEEE_SET_HALTING_MODE(IEEE_ALL, .FALSE.)
+
+        ! Initialize MPI and HDF5
         CALL MPI_Init()
         CALL h5open_f(ierr)
 
+        ! Restore the IEEE halting modes that was selected when the application
+        ! was compiled
+        CALL IEEE_SET_FLAG(IEEE_ALL, .FALSE.)
+        CALL IEEE_SET_HALTING_MODE(IEEE_ALL, saved_fpe_mode)
+
+        ! Need to set the communicators first before set_fp_traps or
+        ! set_underflow_mode since they depend on them
         CALL init_comms()
+
+        ! Allow the user to set desired halting and underflow modes via
+        ! environment variables
+        CALL set_fpe_traps()
+        CALL set_underflow_mode()
+        CALL print_ieee_modes()
+
+        ! Continue MGLET startup
+
         CALL init_precision()
         CALL init_buildinfo()
         CALL init_timer()
@@ -119,5 +147,143 @@ CONTAINS
         CALL h5close_f(ierr)
         CALL MPI_Finalize()
     END SUBROUTINE finish_core
+
+
+    ! Set floating point exceptions handling
+    ! When the environment variable MGLET_FPE_TRAP is set to a string
+    ! containing one or more of the following words, the corresponding
+    ! floating point exception will be enabled:
+    ! - divide_by_zero
+    ! - inexact
+    ! - invalid
+    ! - overflow
+    ! - underflow
+    ! The default is to do nothing (i.e. the method chosen by the compiler is
+    ! used). An emptry string (or absence of any of the keywords above) will
+    ! disable all floating point exceptions.
+    SUBROUTINE set_fpe_traps()
+        USE, INTRINSIC :: IEEE_EXCEPTIONS
+
+        ! Local variables
+        CHARACTER(len=1024) :: mglet_fpe_trap
+        LOGICAL :: found
+
+        ! Get the FPE mode from the environment
+        CALL getenv_char_coll(mglet_fpe_trap, "MGLET_FPE_TRAP", found=found)
+
+        ! When the environment variable is not set, do nothing
+        IF (.NOT. found) RETURN
+
+        ! Start by setting all flags to zero
+        CALL IEEE_SET_HALTING_MODE(IEEE_ALL, .FALSE.)
+
+        ! Convert to lower case
+        mglet_fpe_trap = LOWER(mglet_fpe_trap)
+
+        ! Set the flags according to the environment variable
+        IF (INDEX(mglet_fpe_trap, "divide_by_zero") > 0) THEN
+            CALL IEEE_SET_HALTING_MODE(IEEE_DIVIDE_BY_ZERO, .TRUE.)
+        END IF
+        IF (INDEX(mglet_fpe_trap, "inexact") > 0) THEN
+            CALL IEEE_SET_HALTING_MODE(IEEE_INEXACT, .TRUE.)
+        END IF
+        IF (INDEX(mglet_fpe_trap, "invalid") > 0) THEN
+            CALL IEEE_SET_HALTING_MODE(IEEE_INVALID, .TRUE.)
+        END IF
+        IF (INDEX(mglet_fpe_trap, "overflow") > 0) THEN
+            CALL IEEE_SET_HALTING_MODE(IEEE_OVERFLOW, .TRUE.)
+        END IF
+        IF (INDEX(mglet_fpe_trap, "underflow") > 0) THEN
+            CALL IEEE_SET_HALTING_MODE(IEEE_UNDERFLOW, .TRUE.)
+        END IF
+    END SUBROUTINE set_fpe_traps
+
+
+    ! Set floating point underflow mode
+    ! When the environment variable MGLET_UNDERFLOW is set to "gradual", the
+    ! underflow mode is set to gradual. When it is set to "abrupt", the
+    ! underflow mode is set to abrupt. The default is to do nothing (i.e. the
+    ! method chosen by the compiler is used).
+    SUBROUTINE set_underflow_mode()
+        USE, INTRINSIC :: IEEE_ARITHMETIC
+
+        ! Local variables
+        CHARACTER(len=1024) :: mglet_underflow
+        LOGICAL :: found
+
+        ! Get the underflow mode from the environment
+        CALL getenv_char_coll(mglet_underflow, "MGLET_UNDERFLOW", found=found)
+
+        ! When the environment variable is not set, do nothing
+        IF (.NOT. found) RETURN
+
+        ! Convert to lower case
+        mglet_underflow = LOWER(mglet_underflow)
+
+        ! Set the flags according to the environment variable
+        SELECT CASE(TRIM(mglet_underflow))
+        CASE ("gradual")
+            CALL IEEE_SET_UNDERFLOW_MODE(.TRUE.)
+        CASE ("abrupt")
+            CALL IEEE_SET_UNDERFLOW_MODE(.FALSE.)
+        CASE DEFAULT
+            WRITE(*,*) "Invalid value for MGLET_UNDERFLOW: ", &
+                TRIM(mglet_underflow)
+            CALL errr(__FILE__, __LINE__)
+        END SELECT
+    END SUBROUTINE set_underflow_mode
+
+
+    SUBROUTINE print_ieee_modes()
+        USE, INTRINSIC :: IEEE_ARITHMETIC
+
+        ! Local variables
+        LOGICAL :: flag
+        CHARACTER(len=1024) :: mglet_fpe_trap
+
+        IF (myid /= 0) RETURN
+
+        WRITE(*, '("IEEE aritmetic flags:")')
+
+        CALL IEEE_GET_UNDERFLOW_MODE(flag)
+        IF (flag) THEN
+            WRITE(*, '("    Underflow:     gradual")')
+        ELSE
+            WRITE(*, '("    Underflow:     abrupt")')
+        END IF
+
+        CALL IEEE_GET_HALTING_MODE(IEEE_DIVIDE_BY_ZERO, flag)
+        IF (flag) THEN
+            mglet_fpe_trap = "divide_by_zero"
+        END IF
+
+        CALL IEEE_GET_HALTING_MODE(IEEE_INEXACT, flag)
+        IF (flag) THEN
+            mglet_fpe_trap = TRIM(mglet_fpe_trap) // " inexact"
+        END IF
+
+        CALL IEEE_GET_HALTING_MODE(IEEE_INVALID, flag)
+        IF (flag) THEN
+            mglet_fpe_trap = TRIM(mglet_fpe_trap) // " invalid"
+        END IF
+
+        CALL IEEE_GET_HALTING_MODE(IEEE_OVERFLOW, flag)
+        IF (flag) THEN
+            mglet_fpe_trap = TRIM(mglet_fpe_trap) // " overflow"
+        END IF
+
+        CALL IEEE_GET_HALTING_MODE(IEEE_UNDERFLOW, flag)
+        IF (flag) THEN
+            mglet_fpe_trap = TRIM(mglet_fpe_trap) // " underflow"
+        END IF
+
+        IF (LEN_TRIM(mglet_fpe_trap) > 0) THEN
+            WRITE(*, '("    FPE traps:     ", A)') TRIM(mglet_fpe_trap)
+        ELSE
+            WRITE(*, '("    FPE traps:     none")')
+        END IF
+
+        WRITE(*, '()')
+    END SUBROUTINE print_ieee_modes
 
 END MODULE core_mod
--- src/core/envvars_mod.F90
+++ src/core/envvars_mod.F90
@@ -22,15 +22,19 @@ MODULE envvars_mod
         getenv_bool, getenv_bool_coll
 
 CONTAINS
-    SUBROUTINE getenv_char(envvalue, varname, defaultval)
+    SUBROUTINE getenv_char(envvalue, varname, defaultval, found)
         ! Subroutine arguments
         CHARACTER(len=*), INTENT(OUT) :: envvalue
         CHARACTER(len=*), INTENT(IN) :: varname
         CHARACTER(len=*), INTENT(IN), OPTIONAL :: defaultval
+        LOGICAL, INTENT(OUT), OPTIONAL :: found
 
         ! Local variables
         CHARACTER(len=maxlength) :: prefixname
         INTEGER :: length, status
+        LOGICAL :: found2
+
+        found2 = .FALSE.
 
         IF (LEN(envprefix) + LEN(varname) > maxlength) THEN
             WRITE(*,*) "Too long variable name: ", varname
@@ -40,13 +44,18 @@ CONTAINS
 
         CALL get_environment_variable(prefixname, envvalue, length, status)
         IF (status == 0) THEN
-            ! No error - continue
-            CONTINUE
+            found2 = .TRUE.
         ELSE IF (status == 1 .AND. PRESENT(defaultval)) THEN
             ! Variable not present - but we use default
+            found2 = .FALSE.
             envvalue = defaultval
+        ELSE IF (status == 1 .AND. PRESENT(found)) THEN
+            ! Variable not present - but found is passed so we know
+            found2 = .FALSE.
+            envvalue = REPEAT(" ", LEN(envvalue))
         ELSE IF (status == -1) THEN
             ! Variable contents did not fit into "envvalue"
+            found2 = .TRUE.
             WRITE(*,*) "Error reading: ", varname
             WRITE(*,*) "Has length: ", length
             CALL errr(__FILE__, __LINE__)
@@ -54,10 +63,15 @@ CONTAINS
             WRITE(*,*) "Unknown error, got status: ", status
             CALL errr(__FILE__, __LINE__)
         END IF
+
+        ! Return the found status if required
+        IF (PRESENT(found)) THEN
+            found = found2
+        END IF
     END SUBROUTINE getenv_char
 
 
-    SUBROUTINE getenv_char_coll(envvalue, varname, defaultval)
+    SUBROUTINE getenv_char_coll(envvalue, varname, defaultval, found)
         ! Gets a character environment variable and broadcast it from rank 0
         ! to all other ranks
 
@@ -65,17 +79,21 @@ CONTAINS
         CHARACTER(len=*), INTENT(OUT) :: envvalue
         CHARACTER(len=*), INTENT(IN) :: varname
         CHARACTER(len=*), INTENT(IN), OPTIONAL :: defaultval
+        LOGICAL, INTENT(OUT), OPTIONAL :: found
 
         ! Local variables
         INTEGER(int32) :: nchars
 
         IF (myid == 0) THEN
-            CALL getenv_char(envvalue, varname, defaultval)
-            nchars = LEN(envvalue)
+            CALL getenv_char(envvalue, varname, defaultval, found)
         END IF
 
-        CALL MPI_Bcast(nchars, 1, MPI_INTEGER, 0, MPI_COMM_WORLD)
+        nchars = LEN(envvalue)
         CALL MPI_Bcast(envvalue, nchars, MPI_CHARACTER, 0, MPI_COMM_WORLD)
+
+        IF (PRESENT(found)) THEN
+            CALL MPI_Bcast(found, 1, MPI_LOGICAL, 0, MPI_COMM_WORLD)
+        END IF
     END SUBROUTINE getenv_char_coll
 
 
