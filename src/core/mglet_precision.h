116f58c10c6d804ab9733e929599b9ef4592e235
Håkon Strandenes <h.strandenes@km-turbulenz.no>
2024-10-29 14:30:51 +0100

    Cray compiler fixes

--- src/core/mglet_precision.h
+++ src/core/mglet_precision.h
@@ -22,8 +22,12 @@ typedef long long mgletint;
 
 // If clang is used as a "companion compiler" to NAG nagfor, this will fail or
 // not work. I am not sure if that is supported, though. Usually we use GCC
-// for this. So leave this as is for now.
-#if __NVCOMPILER || __clang__
+// for this.
+// Cray cc/CC also set __clang__ as a macro - but does not behave like it. So
+// We need to check for __cray__ first
+#if __cray__
+#define CFI_type_mgletint CFI_type_long_long
+#elif __NVCOMPILER || __clang__
 #define CFI_type_mgletint CFI_type_int64_t
 #else
 #define CFI_type_mgletint CFI_type_long_long
@@ -34,8 +38,12 @@ typedef int mgletint;
 
 // If clang is used as a "companion compiler" to NAG nagfor, this will fail or
 // not work. I am not sure if that is supported, though. Usually we use GCC
-// for this. So leave this as is for now.
-#if __NVCOMPILER || __clang__
+// for this.
+// Cray cc/CC also set __clang__ as a macro - but does not behave like it. So
+// We need to check for __cray__ first
+#if __cray__
+#define CFI_type_mgletint CFI_type_int
+#elif __NVCOMPILER || __clang__
 #define CFI_type_mgletint CFI_type_int32_t
 #else
 #define CFI_type_mgletint CFI_type_int
