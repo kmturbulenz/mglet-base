bdd9cec15e4f3c79ac745c32bebbbefcd0b8e296
Simon Wenczowski <s.wenczowski@km-turbulenz.de>
2026-02-26 00:30:26 +0100

    fix

--- src/core/omprt_wrapper.h
+++ src/core/omprt_wrapper.h
@@ -7,6 +7,6 @@ void get_num_devices(int*);
 void get_default_device(int*);
 void get_launched_on_gpu(int*);
 
-void get_exp_f(mgletreal*, const mgletreal*);
+void get_exp(mgletreal*, const mgletreal*);
 
 #endif /* __OMPRT_WRAPPER_H__ */
