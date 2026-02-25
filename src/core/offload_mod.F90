66525ee3ae60123d89f282f7b75570c2f1e6c3b7
Simon Wenczowski <s.wenczowski@km-turbulenz.de>
2026-02-25 20:25:13 +0100

    usage in offload_mod

--- src/core/offload_mod.F90
+++ src/core/offload_mod.F90
@@ -1,40 +1,76 @@
 MODULE offload_mod
     USE comms_mod, ONLY: myid
-    USE precision_mod, ONLY: intk
+    USE precision_mod, ONLY: intk, realk, c_intk
+    USE err_mod, ONLY: errr
 
     IMPLICIT NONE(type, external)
     PRIVATE
 
+    INTERFACE
+        ! void get_num_devices(int* n_dev)
+        SUBROUTINE get_num_devices(n_dev) BIND(C)
+            IMPORT :: c_intk
+            INTEGER(kind=c_intk), INTENT(INOUT) :: n_dev
+        END SUBROUTINE get_num_devices
+    END INTERFACE
+
+    INTERFACE
+        ! void get_default_device(int* def_dev)
+        SUBROUTINE get_default_device(def_dev) BIND(C)
+            IMPORT :: c_intk
+            INTEGER(kind=c_intk), INTENT(INOUT) :: def_dev
+        END SUBROUTINE get_default_device
+    END INTERFACE
+
+    INTERFACE
+        ! void get_launched_on_gpuint* l)
+        SUBROUTINE get_launched_on_gpu(l) BIND(C)
+            IMPORT :: c_intk
+            INTEGER(kind=c_intk), INTENT(INOUT) :: l
+            ! 1 for GPU, 0 for CPU
+        END SUBROUTINE get_launched_on_gpu
+    END INTERFACE
+
     ! Public subroutines
     PUBLIC :: init_offload
+
 CONTAINS
+
     SUBROUTINE init_offload()
+
 #ifdef _MGLET_OFFLOAD_
-        USE omp_lib
+
         ! Local variables
         INTEGER(intk) :: num_devices
+        INTEGER(intk) :: gpu_launched
         CHARACTER(len=3) :: message
-        LOGICAL :: is_initial_device
+        REAL(realk) :: a = 1.0
 
-        !$omp target map(from: is_initial_device)
-        is_initial_device = omp_is_initial_device()
-        !$omp end target
+        ! Querying number of devices (via C function)
+        CALL get_num_devices(num_devices)
 
-        IF (is_initial_device) THEN
+        ! Check if a kernel can launch on the GPU (via C function)
+        gpu_launched = -1
+        CALL get_launched_on_gpu(gpu_launched)
+
+        IF (gpu_launched == 1) THEN
+            message = "YES"
+        ELSE IF (gpu_launched == 0) THEN
             message = "NO "
         ELSE
-            message = "YES"
+            CALL errr(__FILE__, __LINE__)
         END IF
 
         IF (myid == 0) THEN
             WRITE(*, '("OPENMP ENABLED")')
             WRITE(*, '("    Offload available:   ", A)') message
-            IF (.NOT. is_initial_device) THEN
-                num_devices = omp_get_num_devices()
+            IF (gpu_launched == 1) THEN
                 WRITE(*, '("    Visible devices (rank 0): ", I0)') num_devices
             END IF
             WRITE(*, '()')
         END IF
 #endif
+
     END SUBROUTINE init_offload
+
 END MODULE offload_mod
