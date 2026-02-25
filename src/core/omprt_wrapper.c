b87dac2ee2aca77839ad3fc976169cdc0fa6b1c3
Simon Wenczowski <s.wenczowski@km-turbulenz.de>
2026-02-25 20:16:39 +0100

    adding functions in C

---
+++ src/core/omprt_wrapper.c
@@ -0,0 +1,33 @@
+
+#include "omprt_wrapper.h"
+
+#include <stdio.h>
+#include <omp.h>
+
+
+void get_num_devices(int* n_dev) {
+    *n_dev = omp_get_num_devices();
+    return;
+}
+
+
+void get_default_device(int* def_dev) {
+    *def_dev = omp_get_default_device();
+    return;
+}
+
+
+void get_launched_on_gpu(int* l) {
+    #pragma omp target map(l)
+    {
+        if (omp_is_initial_device() == 1) {
+            printf("omp_is_initial_device() = 1");
+            *l = 0;     // running on host
+        }
+        else {
+            printf("omp_is_initial_device() = 0");
+            *l = 1;     // running on device
+        }
+    }
+    return;
+}
---
+++ src/core/omprt_wrapper.h
@@ -0,0 +1,8 @@
+#ifndef __OMPRT_WRAPPER_H__
+#define __OMPRT_WRAPPER_H__
+
+void get_num_devices(int*);
+void get_default_device(int*);
+void get_launched_on_gpu(int*);
+
+#endif /* __OMPRT_WRAPPER_H__ */
