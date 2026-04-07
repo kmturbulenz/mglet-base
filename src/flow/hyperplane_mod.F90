1973f2d1ad988c517f957df9ee2cd6c633dded15
Simon Wenczowski <s.wenczowski@km-turbulenz.de>
2026-04-07 18:01:51 +0200

    Contiguous access on the SIP coefficient arrays

--- src/flow/hyperplane_mod.F90
+++ src/flow/hyperplane_mod.F90
@@ -5,8 +5,20 @@ MODULE hyperplane_mod
 
     IMPLICIT NONE
 
+    ! Infrastructure for hyperplane traversal
     TYPE(int_stencils_t), ALLOCATABLE, PROTECTED, TARGET :: mip_sip_list(:)
     TYPE(int_stencils_t), ALLOCATABLE, PROTECTED, TARGET :: idx_sip_list(:)
+
+    ! Coefficients in [L] of ILU (including diagonal)
+    TYPE(real_stencils_t), ALLOCATABLE, PROTECTED, TARGET :: lw_sip_list(:)
+    TYPE(real_stencils_t), ALLOCATABLE, PROTECTED, TARGET :: ls_sip_list(:)
+    TYPE(real_stencils_t), ALLOCATABLE, PROTECTED, TARGET :: lb_sip_list(:)
+    TYPE(real_stencils_t), ALLOCATABLE, PROTECTED, TARGET :: lpr_sip_list(:)
+
+    ! Coefficients in [L] of ILU
+    TYPE(real_stencils_t), ALLOCATABLE, PROTECTED, TARGET :: ue_sip_list(:)
+    TYPE(real_stencils_t), ALLOCATABLE, PROTECTED, TARGET :: un_sip_list(:)
+    TYPE(real_stencils_t), ALLOCATABLE, PROTECTED, TARGET :: ut_sip_list(:)
 
     PUBLIC :: hyperplane_init, hyperplane_finish
 
@@ -16,6 +28,8 @@ CONTAINS
 
         ! Local variables
         INTEGER(intk) :: i, igrid, kk, jj, ii
+        REAL(realk), POINTER, CONTIGUOUS :: lw(:), ls(:), lb(:), lpr(:)
+        REAL(realk), POINTER, CONTIGUOUS :: ue(:), un(:), ut(:)
 
         ! Allocation of the lists of stencils for all grids
         ALLOCATE(mip_sip_list(nmygrids))
@@ -44,7 +58,96 @@ CONTAINS
                 idx_sip_list(i)%arr)
         END DO
 
+        ! Allocation of the lists of stencils for all grids
+        ALLOCATE(lw_sip_list(nmygrids))
+        ALLOCATE(ls_sip_list(nmygrids))
+        ALLOCATE(lb_sip_list(nmygrids))
+        ALLOCATE(lpr_sip_list(nmygrids))
+
+        ALLOCATE(ue_sip_list(nmygrids))
+        ALLOCATE(un_sip_list(nmygrids))
+        ALLOCATE(ut_sip_list(nmygrids))
+
+        ! Iteration over all local grids
+        DO i = 1, nmygrids
+
+            igrid = mygrids(i)
+            CALL get_mgdims(kk, jj, ii, igrid)
+
+            ! Getting the already computed SIP coefficients for one grid
+            CALL get_fieldptr(lw, "SIPLW", igrid)
+            CALL get_fieldptr(ls, "SIPLS", igrid)
+            CALL get_fieldptr(lb, "SIPLB", igrid)
+            CALL get_fieldptr(lpr, "SIPLPR", igrid)
+            CALL get_fieldptr(ue, "SIPUE", igrid)
+            CALL get_fieldptr(un, "SIPUN", igrid)
+            CALL get_fieldptr(ut, "SIPUT", igrid)
+
+            ALLOCATE(lw_sip_list(i)%arr(ii*jj*kk))
+            ALLOCATE(ls_sip_list(i)%arr(ii*jj*kk))
+            ALLOCATE(lb_sip_list(i)%arr(ii*jj*kk))
+            ALLOCATE(lpr_sip_list(i)%arr(ii*jj*kk))
+            ALLOCATE(ue_sip_list(i)%arr(ii*jj*kk))
+            ALLOCATE(un_sip_list(i)%arr(ii*jj*kk))
+            ALLOCATE(ut_sip_list(i)%arr(ii*jj*kk))
+
+            ! Store the SIP coefficients for contiguous memory access
+            CALL reorder_for_access(kk, jj, ii, lw_sip_list(i)%arr, lw, &
+                mip_sip_list(i)%arr, idx_sip_list(i)%arr)
+            CALL reorder_for_access(kk, jj, ii, ls_sip_list(i)%arr, ls, &
+                mip_sip_list(i)%arr, idx_sip_list(i)%arr)
+            CALL reorder_for_access(kk, jj, ii, lb_sip_list(i)%arr, lb, &
+                mip_sip_list(i)%arr, idx_sip_list(i)%arr)
+            CALL reorder_for_access(kk, jj, ii, lpr_sip_list(i)%arr, lpr, &
+                mip_sip_list(i)%arr, idx_sip_list(i)%arr)
+            CALL reorder_for_access(kk, jj, ii, ue_sip_list(i)%arr, ue, &
+                mip_sip_list(i)%arr, idx_sip_list(i)%arr)
+            CALL reorder_for_access(kk, jj, ii, un_sip_list(i)%arr, un, &
+                mip_sip_list(i)%arr, idx_sip_list(i)%arr)
+            CALL reorder_for_access(kk, jj, ii, ut_sip_list(i)%arr, ut, &
+                mip_sip_list(i)%arr, idx_sip_list(i)%arr)
+
+        END DO
+
     END SUBROUTINE hyperplane_init
+
+
+    PURE SUBROUTINE reorder_for_access(kk, jj, ii, acc, arr, mip, idxsip)
+
+        ! Subroutine arguments
+        INTEGER(intk), INTENT(in) :: kk, jj, ii
+        REAL(realk), INTENT(inout) :: acc(kk*jj*ii)
+        REAL(realk), INTENT(in) :: arr(kk*jj*ii)
+
+        ! For the hyperplane traversal
+        INTEGER(intk), INTENT(in) :: mip(ii+jj+kk)
+        INTEGER(intk), INTENT(in) :: idxsip(ii*jj*kk)
+
+        ! Local variables
+        INTEGER(intk) :: n3dmin, n3dmax, m, lm, len, ip, idx, iacc
+
+        ! Subroutine body
+        n3dmin = 3 + 3 + 3
+        n3dmax = (ii-2) + (jj-2) + (kk-2)
+        acc(kk*jj*ii) = -HUGE(1.0_realk)
+
+        ! Iterating over the hyperplanes H(k, j, i) = m
+        DO m = n3dmin, n3dmax
+
+            lm = mip(m)
+            len = mip(m+1) - lm
+
+            ! > Parallel operations on the hyperplane (k, j, i) = m
+            DO ip = 1, len
+
+                iacc = lm + ip
+                idx = idxsip(iacc)
+                acc(iacc) = arr(idx)
+
+            END DO
+        END DO
+
+    END SUBROUTINE reorder_for_access
 
 
 
@@ -58,11 +161,25 @@ CONTAINS
             ! Deallocation of the arrays for one grid
             DEALLOCATE(mip_sip_list(i)%arr)
             DEALLOCATE(idx_sip_list(i)%arr)
+            DEALLOCATE(lw_sip_list(i)%arr)
+            DEALLOCATE(ls_sip_list(i)%arr)
+            DEALLOCATE(lb_sip_list(i)%arr)
+            DEALLOCATE(lpr_sip_list(i)%arr)
+            DEALLOCATE(ue_sip_list(i)%arr)
+            DEALLOCATE(un_sip_list(i)%arr)
+            DEALLOCATE(ut_sip_list(i)%arr)
         END DO
 
         ! Deallocation of the lists of stencils for all grids
         DEALLOCATE(mip_sip_list)
         DEALLOCATE(idx_sip_list)
+        DEALLOCATE(lw_sip_list)
+        DEALLOCATE(ls_sip_list)
+        DEALLOCATE(lb_sip_list)
+        DEALLOCATE(lpr_sip_list)
+        DEALLOCATE(ue_sip_list)
+        DEALLOCATE(un_sip_list)
+        DEALLOCATE(ut_sip_list)
 
     END SUBROUTINE hyperplane_finish
 
@@ -72,7 +189,7 @@ CONTAINS
     SUBROUTINE hyperplane_init_grid(kk, jj, ii, mip, idxsip)
 
         ! Subroutine arguments
-        INTEGER, INTENT(in)    :: kk, jj, ii
+        INTEGER, INTENT(in) :: kk, jj, ii
         INTEGER, INTENT(inout) :: mip(ii+jj+kk), idxsip(ii*jj*kk)
 
         ! Local variables
@@ -121,6 +238,7 @@ CONTAINS
 
                     ! Increment counter
                     ic1 = ic1 + 1
+
                     ! Store linear index
                     CALL sub2ind(idx, k, j, i, kk, jj, ii)
                     idxsip(lm + ic1) = idx
