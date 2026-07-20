e475df5860e05b6107bbf72e2f9b7840b4f8d792
Simon Wenczowski <s.wenczowski@km-turbulenz.de>
2026-07-20 17:54:09 +0200

    Undeclaring mappers

--- src/core/field_t/fieldmapper_mod.F90
+++ src/core/field_t/fieldmapper_mod.F90
@@ -9,10 +9,10 @@ MODULE fieldmapper_mod
     ! Use this module directly. Mapper declarations are not visible through
     ! transitive use statements if any intermediate module is PRIVATE'ized.
     ! Mangle custom mapper-ids to avoid conflicts with variable names.
-    !$omp declare mapper(field_t :: t) map(t%arr, t%buffers, t%ptr, t%length)
-    !$omp declare mapper(field_t__map_arr: field_t :: t) map(t%arr)
-    !$omp declare mapper(field_t__map_buffers: field_t :: t) map(t%buffers)
+    ! $omp declare mapper(field_t :: t) map(t%arr, t%buffers, t%ptr, t%length)
+    ! $omp declare mapper(field_t__map_arr: field_t :: t) map(t%arr)
+    ! $omp declare mapper(field_t__map_buffers: field_t :: t) map(t%buffers)
 
-    !$omp declare mapper(intfield_t :: t) map(t%arr, t%ptr, t%length)
-    !$omp declare mapper(intfield_t__map_arr: intfield_t :: t) map(t%arr)
+    ! $omp declare mapper(intfield_t :: t) map(t%arr, t%ptr, t%length)
+    ! $omp declare mapper(intfield_t__map_arr: intfield_t :: t) map(t%arr)
 END MODULE fieldmapper_mod
