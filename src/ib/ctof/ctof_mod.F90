a03a82a3f868a7128362e1b83fbd7a091ed446bb
Jakob Fuchs <j.fuchs@km-turbulenz.de>
2026-07-28 11:39:47 +0200

    Fix ctof dispatching

--- src/ib/ctof/ctof_mod.F90
+++ src/ib/ctof/ctof_mod.F90
@@ -20,14 +20,21 @@ CONTAINS
         TYPE(field_t), INTENT(in) :: fc_f
         LOGICAL, OPTIONAL, INTENT(in) :: device
 
+#ifdef _MGLET_OFFLOAD_
         ! Local variables
         LOGICAL :: device2
+#endif
 
         ! The coarsest level cannot obtain values from a coarser level
         IF (ilevel == minlevel) RETURN
 
         IF (.NOT. is_ctof_core_init) CALL errr(__FILE__, __LINE__)
 
+        CALL start_timer(230)
+
+#ifdef _MGLET_OFFLOAD_
+        ! If offloading is enabled, the optional device argument determines
+        ! whether device or host version of parent is called.
         IF (PRESENT(device)) THEN
             device2 = device
         ELSE
@@ -35,16 +42,16 @@ CONTAINS
         END IF
 
         IF (device2) THEN
-            ! Use the GPU version
             CALL ctof2(ilevel, ff_f, fc_f)
         ELSE
-#ifdef _MGLET_OFFLOAD_
-            WRITE(*, *) "ctof1 is not supported with offloading"
-            CALL errr(__FILE__, __LINE__)
-#endif
-            ! Use the CPU version
             CALL ctof1(ilevel, ff_f, fc_f)
         END IF
+#else
+        ! Always call ctof1 if offloading is not enabled
+        CALL ctof1(ilevel, ff_f, fc_f)
+#endif
+
+        CALL stop_timer(230)
     END SUBROUTINE ctof
 
 
